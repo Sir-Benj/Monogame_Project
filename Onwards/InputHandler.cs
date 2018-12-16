a1d15403ed66ccfcbb43c72f8769d9adf6a997ab
Sir-Benj <40863501+Sir-Benj@users.noreply.github.com>
2018-12-16 16:31:15 +0000

    changes

---
+++ Onwards/InputHandler.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace Onwards
+{
+    public class InputHandler : Microsoft.Xna.Framework.GameComponent
+    {
+        #region Fields
+
+        static KeyboardState kbState;
+        static KeyboardState kbLastState;
+
+        static GamePadState gpState;
+        static GamePadState gpLastState;
+
+        #endregion
+
+        #region Properties
+        public static KeyboardState KeyboardState
+        {
+            get { return kbState; }
+        }
+
+        public static KeyboardState KeyboardLastState
+        {
+            get { return kbLastState; }
+        }
+
+        public static GamePadState GamepadState
+        {
+            get { return gpState; }
+        }
+
+        public static GamePadState GamepadLastState
+        {
+            get { return gpLastState; }
+        }
+        #endregion
+
+        #region Constructor
+        public InputHandler(Game game) : base(game)
+        {
+            kbState = Keyboard.GetState();
+            gpState = GamePad.GetState(PlayerIndex.One);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            kbLastState = kbState;
+            kbState = Keyboard.GetState();
+
+            gpLastState = gpState;
+            gpState = GamePad.GetState(PlayerIndex.One);
+
+            base.Update(gameTime);
+        }
+        #endregion
+
+        #region General Methods
+        public static void ResetState()
+        {
+            kbLastState = KeyboardState;
+
+            gpState = gpLastState;
+        }
+        #endregion
+
+        #region Keyboard Methods
+        public static bool KeyReleased(Keys key)
+        {
+            return kbState.IsKeyUp(key) && kbLastState.IsKeyDown(key);
+        }
+
+        public static bool KeyPressed(Keys key)
+        {
+            return kbState.IsKeyDown(key) && kbLastState.IsKeyUp(key);
+        }
+
+        public static bool KeyDown(Keys key)
+        {
+            return kbState.IsKeyDown(key);
+        }
+        #endregion
+
+        #region Gamepad Methods
+        public static bool ButtonReleased(Buttons btn)
+        {
+            return gpState.IsButtonUp(btn) && gpLastState.IsButtonDown(btn);
+        }
+
+        public static bool ButtonPressed(Buttons btn)
+        {
+            return gpState.IsButtonDown(btn) && gpLastState.IsButtonUp(btn);
+        }
+
+        public static bool ButtonDown(Buttons btn)
+        {
+            return gpLastState.IsButtonDown(btn);
+        }
+        #endregion
+    }
+}
